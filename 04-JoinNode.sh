e2ac837d270bfc56790b6691660fdfba510b1cb4
Luiz Carlos Kazuyuki Fukaya <35419919+luizcarloskazuyukifukaya@users.noreply.github.com>
2019-05-27 12:49:36 +0900

    Create 04-JoinNode.sh
    Init commit

---
+++ 04-JoinNode.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# TARGET: WORKERS (ONLY Workers VMs)
+
+# Guidance on console only
+echo "Please follow the instruction given on master node setup, which should be something like 'sudo kubeadm jon ....'"
